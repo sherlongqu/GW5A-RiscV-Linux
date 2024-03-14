84de1052d86f67bf6068098b6c7abc5570ff59c7
sherlongqu <sherlongqu@outlook.com>
2024-03-14 18:34:32 +0800

    add ignore file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GW5A-RiscV-Linux
+
+This project is a project using fpga
+
+The purpose is to explore multiple instruction set implementations and run Linux on them
--- example/Hello_world_jlink/Output/Debug/Obj/Hello_world_jlink/riscv_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Hello_world_jlink/riscv_crt0.o: \
- C:\Users\slg\WorkSpace\TangPrimer-25K\LicheeTang25k_VexRV_micro\example\Hello_world_jlink\System\riscv_crt0.s
