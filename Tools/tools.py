dceefe25455ea647b6be7505e817d0160001dbc9
sherlongqu <sherlongqu@outlook.com>
2024-03-14 19:57:52 +0800

    upload tool

--- Tools/tools.py
+++ Tools/tools.py
@@ -1,20 +1,62 @@
-def swap_endianness(data):
-    swapped_data = bytearray()
-    for i in range(0, len(data), 4):
-        chunk = data[i:i+4]
-        swapped_chunk = chunk[::-1]
-        swapped_data.extend(swapped_chunk)
-    return swapped_data
+import os
+import sys
+import time
 
-def convert_byte_to_hex(byte_file, hex_file):
-    with open(byte_file, 'rb') as f_in:
-        with open(hex_file, 'w') as f_out:
-            byte_data = f_in.read()
-            swapped_data = swap_endianness(byte_data)
-            hex_data = swapped_data.hex()
+class fileLocator():
 
-            formatted_hex_data = '\n'.join(hex_data[i:i+8] for i in range(0, len(hex_data), 8))
+    path = str()
 
-            f_out.write(formatted_hex_data)
+    def __init__(self) -> None:
+        path = os.getcwd()
 
-convert_byte_to_hex('Hello_world_jlink.bin', 'Hello_world_jlink.hex')
+class logCreacter():
+    def __init__(self):
+        self.log_file = "log.txt"
+        self.log_file_path = os.path.join(os.getcwd(), self.log_file)
+
+    def log(self, log_message):
+        with open(self.log_file_path, "a") as f:
+            f.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " " + log_message + "\n")
+
+class convertByteToHex:
+
+    @staticmethod
+    def swap_endianness(data):
+        swapped_data = bytearray()
+        for i in range(0, len(data), 4):
+            chunk = data[i:i+4]
+            swapped_chunk = chunk[::-1]
+            swapped_data.extend(swapped_chunk)
+        return swapped_data
+
+    def convert_byte_to_hex(self, byte_file, hex_file):
+        with open(byte_file, 'rb') as f_in:
+            with open(hex_file, 'w') as f_out:
+                byte_data = f_in.read()
+                swapped_data = self.swap_endianness(byte_data)
+                hex_data = swapped_data.hex()
+
+                formatted_hex_data = '\n'.join(hex_data[i:i+8] for i in range(0, len(hex_data), 8))
+
+                f_out.write(formatted_hex_data)
+
+class UI():
+
+    def cil_ui(self):
+        while(True):
+            byte_file = input("Enter the byte file: ")
+            hex_file = input("Enter the hex file: ")
+
+            convertByteToHex().convert_byte_to_hex(byte_file, hex_file)
+
+            print("Conversion complete")
+
+            cont = input("Do you want to convert another file? (y/n): ")
+            if cont.lower() != 'y':
+                break
+
+def main():
+    UI().cil_ui()
+
+if __name__ == "__main__": 
+    main()
